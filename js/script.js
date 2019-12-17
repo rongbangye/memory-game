f1cfd6093017c5e32364efd781c4fb70168bc15d
rongbang <rongbang.ye@gmail.com>
2019-12-17 15:38:40 -0800

    version 0.9 completed

--- js/script.js
+++ js/script.js
@@ -50,40 +50,33 @@ const cardsArray = [
       name: 'goomba',
       img: 'img/goomba.png',
     },
-  ]
+  ];
 
   // grab the div with the an id of root - game
-  const game = document.getElementById('game')
-
+  const game = document.getElementById('game');
   // create a section with a class of grind
-  const grid = document.createElement('section')
-  grid.setAttribute('class', 'grid')
-
+  const grid = document.createElement('section');
+  grid.setAttribute('class', 'grid');
   // Append the grid section to the game div
-  game.appendChild(grid)
+  game.appendChild(grid);
 
-  // for each item in the cardsArray
-  // cardsArray.forEach(item => {
-  //   const card = document.createElement('div')
-
-  //   card.classList.add('card')
-
-  //   // set the data-name attribute of the div to the cardsArray name
-  //   card.dataset.name = item.name
-
-  //   card.style.backgroundImage = `url(${item.img})`
-
-  //   // append the div to the grind section
-  //   grid.appendChild(card)
-  // })
+/*
+// for each item in the cardsArray
+  cardsArray.forEach(item => {
+  const card = document.createElement('div')
+  card.classList.add('card')
+//set the data-name attribute of the div to the cardsArray name
+   card.dataset.name = item.name
+   card.style.backgroundImage = `url(${item.img})`
+//append the div to the grind section
+     grid.appendChild(card)
+   })
+*/
 
   // duplicate the cardsArray array that match for each card
   let gameGrid = cardsArray.concat(cardsArray)
-
-  // Randomize the display of cards 
-  // shuffle the array using sort() and Math.random()
+  // Randomize the display of cards and shuffle the array using sort() and Math.random()
   gameGrid.sort(() => Math.random() - 0.5);
-
 
   // for reach item in the gameGrib array
   gameGrid.forEach(item => {
@@ -99,46 +92,41 @@ const cardsArray = [
     const back = document.createElement('div')
     back.classList.add('back')
 
-
-
     card.style.backgroundImage = `url(${item.img})`
 
     // append the div to the grind section
     grid.appendChild(card)
     card.appendChild(front)
     card.appendChild(back)
-  })
+  });
 
 // Only allow two cards to be selected at a time
 // we need to store the guesses and counter
-let count = 0
+let count = 0;
 // add a place to store the first and second guessess 
-let firstGuess = ''
-let secondGuess = ''
+let firstGuess = '';
+let secondGuess = '';
 // to fix click on the same element twice, use previousTarget
-let previousTarget = null
- 
-let delay = 1200
+let previousTarget = null;
+let delay = 1200;
 
 // add an event listener to the entire grid.
 // anytime an element is clicked, the selected class will be apply
 grid.addEventListener('click', function(event) {
   // the event target is our clicked item
-  let clicked = event.target
-
-  // add the check to our return 
-  if (clicked.nodeName === 'SECTION' || 
-      clicked === previousTarget ||
-      clicked.parentNode.classList.contains('selected')
-  ){
-    return
-  }
+  const clicked = event.target;
 
   // Do not allow the grid section itself to be selected
   // only select divs inside the grid
-  if (clicked.nodeName === 'SECTION') {
-    return
+  // add the check to our return 
+  if (clicked.nodeName === 'SECTION' || 
+      clicked === previousTarget ||
+      clicked.parentNode.classList.contains('selected') ||
+      clicked.parentNode.classList.contains('match')
+  ){
+    return;
   }
+
 
   // add selected class
   // clicked.classList.add('selected')
@@ -160,20 +148,20 @@ grid.addEventListener('click', function(event) {
       // if the first guess match the second guess
       if (firstGuess === secondGuess) {
         // run match function
-        match();
-        resetGuessess();
+        setTimeout(match, delay);
       } else {
-        resetGuessess();
+        setTimeout(resetGuessess, delay);
       }
     }
 
-    // add delay
+/*   // add delay
     if (firstGuess === secondGuess) {
       setTimeout(match, delay)
       setTimeout(resetGuessess, delay)
     } else {
       setTimeout(resetGuessess, delay)
     }
+*/
       // assign the clicked value to the previousTarget after the first click
       previousTarget = clicked;
   }
@@ -185,20 +173,21 @@ const match = () => {
   var selected = document.querySelectorAll('.selected')
   selected.forEach(card => {
     card.classList.add('match')
-  })
-}
+  });
+};
 
 // function to reset the guess count after two guessess. 
 const resetGuessess = () => {
-  firstGuess = ''
-  secondGuess = ''
-  count = 0
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
 
   var selected = document.querySelectorAll('.selected')
   selected.forEach(card => {
-    card.classList.remove('selected')
-  })
-}
+    card.classList.remove('selected');
+  });
+};
 
 
 
